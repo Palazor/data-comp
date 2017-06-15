e5b8534605df646d5b5604af0efc2c5defca9e39
Palazor <renze1983@163.com>
2017-06-15 19:33:41 +0800

    Formatted

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 from sklearn import svm
+from sklearn.externals import joblib
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import precision_score
@@ -59,19 +60,11 @@ features_train = features_pd.iloc[:train_count]
 # print(features_train.describe())
 features_test = features_pd.iloc[train_count:]
 # print(features_test.describe())
-x_train, x_test, y_train, y_test = train_test_split(features_train, labels, test_size=0.2, random_state=2)
+x_train, x_test, y_train, y_test = train_test_split(features_train, labels, test_size=0.2)#, random_state=1)
 X = (x_train, x_test, y_train, y_test)
-
-svc = svm.SVC(kernel='rbf', C=10, random_state=1, gamma=0.1, max_iter=1000)
-linear_svc = svm.LinearSVC(C=10, random_state=1, max_iter=100)
-pred = train_model(svc, X).predict(features_test)
-# print(pred, len(pred), pred.mean())
-train_model(linear_svc, X)
 
 # print(cross_val_score(svc, features_train, labels, scoring="neg_mean_squared_error", cv=10).mean())
 # print(cross_val_score(linear_svc, features_train, labels, scoring="neg_mean_squared_error", cv=10).mean())
-
-
 
 dsTrain = ClassificationDataSet(18, 1, nb_classes=2)
 rows = len(x_train)
@@ -85,39 +78,41 @@ for row in range(rows):
     dsTest.addSample(tuple(x_test.iloc[row]), y_test.iloc[row])
 dsTest._convertToOneOfMany()
 
-if True:
+svc = None
+fnn = None
+if False:
+    svc = svm.SVC(kernel='rbf', C=10, random_state=1, gamma=0.1, max_iter=1000)
+    linear_svc = svm.LinearSVC(C=10, random_state=1, max_iter=100)
+    pred = train_model(svc, X).predict(features_test)
+    # print(pred, len(pred), pred.mean())
+    train_model(linear_svc, X)
+    joblib.dump(svc, 'svc.pkl')
+
     fnn = buildNetwork(18, 15, 2, outclass=SoftmaxLayer)
     trainer = BackpropTrainer(fnn, dataset=dsTrain, momentum=0, verbose=False, weightdecay=0.0)
-
-    # trainer.trainEpochs(1000)
     trainer.trainUntilConvergence(maxEpochs=100000)
-    NetworkWriter.writeToFile(fnn, 'filename.xml')
+    NetworkWriter.writeToFile(fnn, 'ann.xml')
 else:
-    fnn = NetworkReader.readFrom('filename.xml')
+    svc = joblib.load('svc.pkl')
+
+    fnn = NetworkReader.readFrom('ann.xml')
     trainer = BackpropTrainer(fnn, dataset=dsTrain, momentum=0, verbose=False, weightdecay=0.0)
 
 trnresult = percentError(trainer.testOnClassData(), dsTrain['target'])
 testResult = percentError(trainer.testOnClassData(dataset=dsTest), dsTest['target'])
 # print("epoch: {}\ntrain error: {}\ntest error: {}".format(trainer.totalepochs, trnresult, testResult))
 
-res = trainer.testOnClassData(dataset=dsTest)
-out = fnn.activateOnDataset(dsTest).argmax(axis=1)
-tgt = [int(x[1]) for x in dsTest['target']]
-# print(accuracy_score(tgt, res),
-#       precision_score(tgt, res),
-#       recall_score(tgt, res),
-#       f1_score(tgt, res))
+tgt_tst = [int(x[1]) for x in dsTest['target']]
+out_tst = fnn.activateOnDataset(dsTest).argmax(axis=1)
+prd_tst = svc.predict(x_test)
+svc_f1_tst = f1_score(tgt_tst, prd_tst)
+fnn_f1_tst = f1_score(tgt_tst, out_tst)
 
-pred = train_model(svc, X).predict(x_test)
-print('Test', f1_score(tgt, pred), f1_score(tgt, res), f1_score(tgt, out))
-
-res = trainer.testOnClassData(dataset=dsTrain)
-out = fnn.activateOnDataset(dsTrain).argmax(axis=1)
-tgt = [int(x[1]) for x in dsTrain['target']]
-# print(accuracy_score(tgt, res),
-#       precision_score(tgt, res),
-#       recall_score(tgt, res),
-#       f1_score(tgt, res))
-
-pred = train_model(svc, X).predict(x_train)
-print('Train', f1_score(tgt, pred), f1_score(tgt, res), f1_score(tgt, out))
+tgt_trn = [int(x[1]) for x in dsTrain['target']]
+out_trn = fnn.activateOnDataset(dsTrain).argmax(axis=1)
+prd_trn = svc.predict(x_train)
+svc_f1_trn = f1_score(tgt_trn, prd_trn)
+fnn_f1_trn = f1_score(tgt_trn, out_trn)
+print("\t\t  SVC\t\t  ANN\t\tWIN")
+print('Test\t{0:.5}\t\t{1:.5}\t\t{2}'.format(svc_f1_tst, fnn_f1_tst, 'SVC' if svc_f1_tst > fnn_f1_tst else 'FNN'))
+print('Train\t{0:.5}\t\t{1:.5}\t\t{2}'.format(svc_f1_trn, fnn_f1_trn, 'SVC' if svc_f1_trn > fnn_f1_trn else 'FNN'))
